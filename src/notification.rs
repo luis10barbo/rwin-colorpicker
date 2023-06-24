5d93ec696c51d88a38e12eb84f36082ce586d004
luis10barbo <luis10barbo2020@gmail.com>
2023-06-24 01:04:52 -0300

    Easily toggable notifs for devlopment

--- src/notification.rs
+++ src/notification.rs
@@ -1,6 +1,10 @@
 use notify_rust::Notification;
-
+const NOTIF_ENABLED: bool = false;
 pub fn create_picker_notification(body: &str) {
+    if !NOTIF_ENABLED {
+        return;
+    }
+
     Notification::new()
         .summary("Color Picker")
         .appname("color_picker_app")
